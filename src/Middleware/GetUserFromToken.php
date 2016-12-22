362bec983be8e2b7b6f08c47539ee3f052c42045
Shashi Prakash Gautam <shashi@nineleaps.com>
2016-12-22 13:15:18 +0530

    set status code

--- src/Middleware/GetUserFromToken.php
+++ src/Middleware/GetUserFromToken.php
@@ -27,21 +27,21 @@ class GetUserFromToken extends BaseMiddleware
     {
         $token = $request->header('Authorization');
         if (! $token){
-            return response()->json(['status'=>'error','error'=>['message'=>'token not provided']]);
+            return response()->json(['status'=>'error','error'=>['message'=>'token not provided']],401);
         }
 
         try {
             $user = $this->auth->authenticate($token);
         } catch (TokenExpiredException $e) {
-            return response()->json(['status'=>'error','error'=>['message'=>'token expired']]);
+            return response()->json(['status'=>'error','error'=>['message'=>'token expired']],401);
             //return $this->respond('tymon.jwt.expired', 'token_expired', $e->getStatusCode(), [$e]);
         } catch (JWTException $e) {
-            return response()->json(['status'=>'error','error'=>['message'=>'Invalid token']]);
+            return response()->json(['status'=>'error','error'=>['message'=>'Invalid token']],401);
             //return $this->respond('tymon.jwt.invalid', 'token_invalid', $e->getStatusCode(), [$e]);
         }
 
         if (! $user) {
-            return response()->json(['status'=>'error','error'=>['message'=>'Unauthorised']]);
+            return response()->json(['status'=>'error','error'=>['message'=>'Unauthorised']],401);
         }
 
         $this->events->fire('tymon.jwt.valid', $user);
